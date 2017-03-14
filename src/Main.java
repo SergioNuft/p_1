9b8e81604f08aa96c3a1dbccfde240611285799e
SergioNuft <sergio_nuft@i.ua>
2017-03-14 22:30:02 +0200

    Annnny TEST BABY

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import pany.Dusy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i=0;
+
+        System.out.println(56.9);
+
+
+
+        if (i==2)
+            System.out.println("True!");
+        else
+            System.out.println("False!");
+
+        //for (i)
+
+        System.out.println("Hello World!");
+        Dusy.pr();
+    }
+
+}
---
+++ src/pany/Dusy.java
@@ -0,0 +1,7 @@
+package pany;
+
+public class Dusy {
+    public static void pr() {
+        System.out.println("Any pany.Dusy!");
+    }
+}
